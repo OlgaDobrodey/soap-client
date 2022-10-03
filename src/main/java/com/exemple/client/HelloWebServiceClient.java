aa68bd036c007b338cbc25cc817d39f8c1c1b24e
Olga Dobrodey <olga.gurkovskaya@gmail.com>
2022-10-03 13:09:20 +0300

    corrected

--- src/main/java/com/exemple/client/HelloWebServiceClient.java
+++ src/main/java/com/exemple/client/HelloWebServiceClient.java
@@ -12,18 +12,9 @@ import java.net.URL;
 public class HelloWebServiceClient {
 
     public static void main(String[] args) throws MalformedURLException {
-//        // создаем ссылку на wsdl описание
-//        URL url = new URL("http://localhost:8081/wss/first?wsdl");
-//
-//        // Параметры следующего конструктора смотрим в самом первом теге WSDL описания - definitions
-//        // 1-ый аргумент смотрим в атрибуте targetNamespace
-//        // 2-ой аргумент смотрим в атрибуте name
-//        QName qname = new QName("http://server.dobrodey.ru/", "HelloWebServiceImplService");
-//
-//        // Теперь мы можем дотянуться до тега service в wsdl описании,
-//        Service service = Service.create(url, qname);
-
+        // создаем ссылку на wsdl описание
         URL url = new URL("http://localhost:8081/wss/first?wsdl");
+
         HelloWebServiceImplService servicehe = new HelloWebServiceImplService(url);
         HelloWebService hello = servicehe.getHelloWebServiceImplPort();
 //
