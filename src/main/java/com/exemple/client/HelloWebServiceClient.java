2947f0ee7f6c8be8a8dbc8d79482d9ad550b0233
Olga Dobrodey <olga.gurkovskaya@gmail.com>
2022-10-03 13:07:44 +0300

    new

--- src/main/java/com/exemple/client/HelloWebServiceClient.java
+++ src/main/java/com/exemple/client/HelloWebServiceClient.java
@@ -3,6 +3,7 @@ package com.exemple.client;
 import com.exemple.generate.HelloWebService;
 import com.exemple.generate.HelloWebServiceImplService;
 import com.exemple.generate.Users;
+import com.exemple.model.User;
 
 import javax.xml.namespace.QName;
 import java.net.MalformedURLException;
@@ -23,15 +24,22 @@ public class HelloWebServiceClient {
 //        Service service = Service.create(url, qname);
 
         URL url = new URL("http://localhost:8081/wss/first?wsdl");
-        QName qname = new QName("http://server.dobrodey.ru/", "HelloWebServiceImplService");
         HelloWebServiceImplService servicehe = new HelloWebServiceImplService(url);
         HelloWebService hello = servicehe.getHelloWebServiceImplPort();
 //
 //        // Ура! Теперь можно вызывать удаленный метод
-        System.out.println(hello.getHelloString("Olga"));
-        System.out.println(hello.getList("Olha"));
-        Users users = hello.getUser();
-        System.out.println(users);
-        System.out.println(hello.add(1,3));
+        System.out.println("Hello OLga: "+hello.getHelloString("Olga"));
+        System.out.println("Print list string"+hello.getList("Olha").getItem());
+        System.out.println("Print list string"+hello.getList("Olha"));
+
+        System.out.println("Print user: "+hello.getUser().toString());
+        User user = new User(hello.getUser().getId(), hello.getUser().getName());
+        System.out.println(user);
+
+        System.out.println("Print user Name: "+hello.getUser().getName());
+        System.out.println("Print summa: "+hello.add(1,3));
+        System.out.print("List of String: "+ hello.getList1("1").getItem());
+        System.out.print("List of Users: ");
+        hello.getList1("1").getItem().stream().forEach(users1 -> System.out.println(users1.getName()+ " "+ users1.getId()));
     }
 }
---
+++ src/main/java/com/exemple/model/User.java
@@ -0,0 +1,17 @@
+package com.exemple.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    protected Integer id;
+    protected String name;
+}
